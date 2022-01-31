843e8cea2eb1109799e0058429d6478851cea6ac
ehl6 <ehl6@rstudio.cs.calvin.edu>
2022-01-31 14:55:56 -0500

    fixing warnings

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,6 +1,8 @@
 #' Split a string
 #'
-#' @param x A character vector with, at most, one element.
+#' @param string A character vector with, at most, one element.
+#' @param pattern The character to split on.
+#' @param n Some number for something.
 #' @inheritParams stringr::str_split
 #'
 #' @return A character vector.
